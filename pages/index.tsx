960c5074b15610f5e8cee0b92b865eaadc247e4b
Tsubasa Endo <tsubasa.bneaus@gmail.com>
2022-12-02 17:43:31 +1000

    add portfolio and about pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,33 +1,19 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import Image from "next/image";
+import profileImage from "../public/profile.jpg";
 
 export default function Home() {
   return (
     <div>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
-      <h1>Hello</h1>
+      <div className="introduction">
+        <h1>Tsubasa Endo</h1>
+        <Image className="profileImage" src={profileImage} alt="" />
+        <p>2nd year university student majoring Computer Science</p>
+      </div>
+      <div className="introduction">
+        <h1>Portfolio</h1>
+        <p>Uploading Soon...</p>
+      </div>
     </div>
   );
 }
--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -1,5 +1,9 @@
 const Portfolio = () => {
-  return;
+  return (
+    <div className="portfolio">
+      <h1>Portfolio</h1>
+    </div>
+  );
 };
 
 export default Portfolio;
