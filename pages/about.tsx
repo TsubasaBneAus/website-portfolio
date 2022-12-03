bbea36baf146ebf942c26b4d6a339d83cf4bb7af
Tsubasa Endo <tsubasa.bneaus@gmail.com>
2022-12-03 20:38:31 +1000

    modify font and layout

--- pages/about.tsx
+++ pages/about.tsx
@@ -5,34 +5,38 @@ import { NextPage } from "next";
 
 const About: NextPage = () => {
   return (
-    <div className="about">
-      <h1>About</h1>
-      <p className="description">
-        I am a second year university student currently specialising in Computer
-        Science at Queensland University of Technology in Australia. I have
-        various experiences of programming languages, for instance, Python
-        (pandas etc.), R, Java, JavaScript (TypeScript, React, Next.js), Node.js
-        (Express), SQL (MySQL, SQLite etc.) C, C#.
-      </p>
-      <h1>Contact</h1>
+    <div className="fadein">
+      <div className="about">
+        <h1>About</h1>
+        <p className="description">
+          I am a second year university student currently specialising in
+          Computer Science at Queensland University of Technology in Australia.
+          I have various experiences of programming languages, for instance,
+          Python (pandas etc.), R, Java, HTML, CSS, JavaScript (TypeScript, React,
+          Next.js), Node.js (Express), SQL (MySQL, SQLite etc.), Assembly, C, C#.
+        </p>
+        <h1>Contact</h1>
 
-      <p className="contact">
-        <span className="spacedText">tsubasa.bneaus@gmail.com</span> <HiMail />
-      </p>
-      <Link
-        className="contact"
-        target="_blank"
-        href="https://www.linkedin.com/in/tsubasa-endo-421b4b1a6"
-      >
-        <span className="spacedText">LinkedIn</span> <FaLinkedin />
-      </Link>
-      <Link
-        className="contact"
-        target="_blank"
-        href="https://www.facebook.com/tsubasa.endo.376"
-      >
-        <span className="spacedText">Facebook</span> <FaFacebookSquare />
-      </Link>
+        <p className="contact">
+          <HiMail />
+          <span className="spacedText">tsubasa.bneaus@gmail.com</span>{" "}
+        </p>
+        <Link
+          className="contact"
+          target="_blank"
+          href="https://www.linkedin.com/in/tsubasa-endo-421b4b1a6"
+        >
+          <FaLinkedin /> <span className="spacedText">LinkedIn</span>
+        </Link>
+        <Link
+          className="contact"
+          target="_blank"
+          href="https://www.facebook.com/tsubasa.endo.376"
+        >
+          <FaFacebookSquare />
+          <span className="spacedText">Facebook</span>
+        </Link>
+      </div>
     </div>
   );
 };
